8090cc6b69ce830442b60c26559da0d4afddb566
anasthokan <anasthokan0@gmail.com>
2025-11-12 10:25:41 +0530

    ipcont

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,7 +9,10 @@ export default function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/contact", form);
+      const API_URL =
+      process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+      await axios.post(`${API_URL}/api/contact`, form);
       setStatus("✅ Message sent successfully!");
       setForm({ name: "", email: "", message: "" });
     } catch {
